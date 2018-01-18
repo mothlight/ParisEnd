faf454ce480ae2ecb1d2c953b7b1788edab056ea
Kerry <kerry.nice@monash.edu>
2018-01-18 18:31:58 +1100

    Change more plots based on Jasper's comments

--- Article/compileArticle.sh
+++ Article/compileArticle.sh
@@ -1,15 +1,15 @@
 cp -u ../../Bibtex/PhDMendeley.bib/library.bib .
 
 cp -u ../analysis_minus_sydmel_maps/MelbourneOverall.png Images/MelbourneOverall_maps.png
-cp -u ../analysis_minus_sydmel_maps/MelbourneOverallAbrev.png Images/MelbourneOverallAbrev_maps.png
+cp -u ../analysis_minus_sydmel_maps/MelbourneOverallAbrev2.png Images/MelbourneOverallAbrev_maps.png
 cp -u ../analysis_minus_sydmel_maps/SydneyOverall.png Images/SydneyOverall_maps.png
-cp -u ../analysis_minus_sydmel_maps/SydneyOverallAbrev.png Images/SydneyOverallAbrev_maps.png
+cp -u ../analysis_minus_sydmel_maps/SydneyOverallAbrev2.png Images/SydneyOverallAbrev_maps.png
 
 
 cp -u ../analysis_minus_sydmel_satellite/MelbourneOverall.png Images/MelbourneOverall_sat.png
-cp -u ../analysis_minus_sydmel_satellite/MelbourneOverallAbrev.png Images/MelbourneOverallAbrev_sat.png
+cp -u ../analysis_minus_sydmel_satellite/MelbourneOverallAbrev2.png Images/MelbourneOverallAbrev_sat.png
 cp -u ../analysis_minus_sydmel_satellite/SydneyOverall.png Images/SydneyOverall_sat.png
-cp -u ../analysis_minus_sydmel_satellite/SydneyOverallAbrev.png Images/SydneyOverallAbrev_sat.png
+cp -u ../analysis_minus_sydmel_satellite/SydneyOverallAbrev2.png Images/SydneyOverallAbrev_sat.png
 
 
 cp -u ../analysis_minus_sydmel_street_view/MelbourneOverall.png Images/MelbourneOverall_street.png
